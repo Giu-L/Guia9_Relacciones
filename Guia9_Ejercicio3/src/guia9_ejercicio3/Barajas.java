589abe6015ae2e06468c5c4a19d2846dd3a0e1a3
Giu-L <98965749+Giu-L@users.noreply.github.com>
2023-03-23 22:02:31 -0300

    Add files via upload

---
+++ Guia9_Ejercicio3/src/guia9_ejercicio3/Barajas.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia9_ejercicio3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ *
+ * @author Usuario
+ */
+/*
+Realizar una baraja de cartas españolas orientada a objetos. Una carta tiene un número entre 1
+y 12 (el 8 y el 9 no los incluimos) y un palo (espadas, bastos, oros y copas). Esta clase debe
+contener un método toString() que retorne el número de carta y el palo. La baraja estará
+compuesta por un conjunto de cartas, 40 exactamente.
+*/
+public class Barajas {
+    public ArrayList<Carta> cartas;
+     
+    
+    
+    public Barajas(){   
+        //lista de crtas
+      this.cartas = new ArrayList<Carta>(40);
+        String[] palos = { "ORO","ESPADA","COPA","BASTO"};
+        // se crea una lista de palos y see dice que va entre 0-13 obviando los 8y9
+        for (String aux : palos) {
+            for (int i = 0; i < 10; i++) {
+                    if(i!=8 && i!=9){
+                Carta carta = new Carta(i, aux);
+                this.cartas.add(carta);
+            }
+                    System.out.println(aux + i);
+            }
+            
+        }
+                
+               
+                }
+
+            }
+        
+        
+    
+    
+    
+
---
+++ Guia9_Ejercicio3/src/guia9_ejercicio3/Guia9_Ejercicio3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia9_ejercicio3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Guia9_Ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Barajas bara = new Barajas();
+    
+        }
+    
+}
